d8f236d6d98f6fe2633ff17ef9434c07a4d196a8
Anastasiya <kabakova.nas@gmail.com>
2023-02-01 00:02:15 +0300

    "edit main"

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // объявление начальной суммы счёта
+        double AccountScore = 7890.99;
+        // объявление суммы пополнения
+        double Replenishment = 4569;
+        double Bonus = 0;
+        // проверка условия
+        if(Replenishment>1000){
+            Bonus = (int)Replenishment / 100;
+        }
+
+        System.out.println(Bonus  + " - Количество бонусных рублей.");
+        System.out.println(AccountScore+Bonus  + " - итоговый счёт.");
     }
 }
